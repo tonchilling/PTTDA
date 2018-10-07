0197f994bc583b8fbc66f11dd215e1b4c43d9017
kahnkaew <praisan.iphone@gmail.com>
2018-10-06 18:36:42 -0700

    API get all region pipeline

---
+++ API/Controllers/Master/M_RegionPipelineController.cs
@@ -0,0 +1,45 @@
+﻿using BAL.PTT.Master;
+using DTO.PTT.Master;
+using DTO.Util;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    [RoutePrefix("api/RegionPipeline")]
+    public class M_RegionPipelineController : ApiController
+    {
+        Logger logger = new Logger("M_RegionPipelineController");
+        M_RegionPipelineBAL bal = null;
+
+        [HttpPost]
+        [Route("GetAll")]
+        public HttpResponseMessage GetAll()
+        {
+            bal = new M_RegionPipelineBAL();
+            ResposeType response = new ResposeType();
+            HttpResponseMessage mapMessage = null;
+            List<M_RegionPipelineDTO> dataList = new List<M_RegionPipelineDTO>();
+            
+            response.statusCode = false;
+            try
+            {
+                dataList = bal.getRegionPipeline();
+                response.statusCode = true;
+                response.data = dataList;
+            }
+            catch (Exception ex)
+            {
+                logger.error("M_RegionPipelineController GetAll error:" + ex.ToString());
+                response.statusText = ex.ToString();
+            }
+
+            mapMessage = Request.CreateResponse(HttpStatusCode.OK, response);
+            return mapMessage;
+        }        
+    }
+}
---
+++ BAL/PTT/Master/M_RegionPipelineBAL.cs
@@ -0,0 +1,64 @@
+﻿using DAO.PTT.Master;
+using DTO.PTT.Master;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace BAL.PTT.Master
+{
+    public class M_RegionPipelineBAL : BaseBL
+    {
+        M_RegionPipelineDAO dao = null;
+        public M_RegionPipelineBAL()
+        {
+            dao = new M_RegionPipelineDAO();
+        }
+
+        public override bool Action()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Add(object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Delete(object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataTable FindByAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataTable FindByColumn(object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Update(object data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<M_RegionPipelineDTO> getRegionPipeline()
+        {
+            List<M_RegionPipelineDTO> objList = new List<M_RegionPipelineDTO>();
+            try
+            {
+                objList = dao.getRegionPipeline();
+            }
+            catch (Exception ex)
+            {
+                Log("getRegionPipeline", "Error", ex.ToString());
+            }
+            return objList;
+        }
+    }
+}
